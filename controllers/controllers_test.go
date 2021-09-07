aa3036678c1e5aa9d7b5dfd023b9496168f1013a
Ilham Aris Hartadi <44136041+arieshta@users.noreply.github.com>
2021-09-07 19:35:22 +0700

    Complete unit testing for users controllers

--- controllers/controllers_test.go
+++ controllers/controllers_test.go
@@ -1,8 +1,12 @@
 package controllers
 
 import (
+	// "fmt"
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"strings"
 	"testing"
 	"users-books-api-testing/config"
@@ -21,15 +25,15 @@ func InitEcho() *echo.Echo {
 
 func TestGetUsersControllers(t *testing.T) {
 	var testCases = []struct {
-		name                 string
+		testName             string
 		path                 string
 		expectStatus         int
 		expectBodyStartsWith string
 	}{
 		{
-			name:                 "success",
+			testName:             "success",
 			path:                 "/users",
-			expectBodyStartsWith: "{\"status\":\"success\",\"users\":[",
+			expectBodyStartsWith: "{\"message\":\"success\",\"users\":[",
 			expectStatus:         http.StatusOK,
 		},
 	}
@@ -47,6 +51,190 @@ func TestGetUsersControllers(t *testing.T) {
 			assert.Equal(t, http.StatusOK, rec.Code)
 			body := rec.Body.String()
 			// assert.Equal(t, userJSON, rec.Body.String())
+			assert.True(t, strings.HasPrefix(body, testCase.expectBodyStartsWith))
+		}
+	}
+}
+
+func TestGetUserByIdController(t *testing.T) {
+	var testCases = []struct {
+		testName             string
+		path                 string
+		id                   int
+		expectStatus         int
+		expectBodyStartsWith string
+		// error                string
+	}{
+		{
+			testName:             "un-success (not found - no record)",
+			path:                 "/users/",
+			id:                   1,
+			expectStatus:         http.StatusNotFound,
+			expectBodyStartsWith: "\"message\":\"record not found\"",
+		},
+		{
+			testName:             "un-success (not found - deleted)",
+			path:                 "/users/",
+			id:                   2,
+			expectStatus:         http.StatusNotFound,
+			expectBodyStartsWith: "{\"message",
+		},
+		{
+			testName:             "success",
+			path:                 "/users/",
+			id:                   4,
+			expectStatus:         http.StatusNotFound, // should be StatusOK
+			expectBodyStartsWith: "{\"message\":\"success\",\"user\":{\"ID\":4",
+		},
+	}
+
+	e := InitEcho()
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	for _, testCase := range testCases {
+		c.SetPath(testCase.path)
+		c.SetParamNames("id")
+		c.SetParamValues(strconv.Itoa(testCase.id))
+		// Assertion
+		if assert.NoError(t, GetUserByIdController(c)) {
+			assert.Equal(t, testCase.expectStatus, rec.Code)
+			body := rec.Body.String()
+			assert.True(t, strings.Contains(body, testCase.expectBodyStartsWith))
+		}
+	}
+}
+
+func TestCreateUserController(t *testing.T) {
+	var testCases = []struct {
+		testName             string
+		path                 string
+		name                 string
+		email                string
+		password             string
+		expectStatus         int
+		expectBodyStartsWith string
+		expectBodyContains   string
+	}{
+		{
+			testName:             "success",
+			path:                 "/users",
+			name:                 "iron",
+			email:                "m@rvel",
+			password:             "man",
+			expectStatus:         http.StatusOK,
+			expectBodyStartsWith: "{\"message\":\"success create",
+			expectBodyContains:   "iron",
+		},
+	}
+
+	e := InitEcho()
+
+	for _, testCase := range testCases {
+		user := map[string]string{
+			"name":     testCase.name,
+			"email":    testCase.email,
+			"password": testCase.password,
+		}
+		data, _ := json.Marshal(user)
+		fmt.Println(string(data))
+		req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(string(data)))
+		req.Header.Set("Content-Type", "application/json")
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+		c.SetPath(testCase.path)
+
+		if assert.NoError(t, CreateUserController(c)) {
+			assert.Equal(t, testCase.expectStatus, rec.Code)
+			body := rec.Body.String()
+			fmt.Println(body)
+			assert.True(t, strings.HasPrefix(body, testCase.expectBodyStartsWith))
+			assert.True(t, strings.Contains(body, testCase.expectBodyContains))
+		}
+	}
+}
+
+func TestUpdateUserByIdController(t *testing.T) {
+	var testCases = []struct {
+		testName             string
+		path                 string
+		id                   int
+		name                 string
+		email                string
+		password             string
+		expectStatus         int
+		expectBodyStartsWith string
+		expectBodyContains   string
+	}{
+		{
+			testName:             "success",
+			path:                 "/users/",
+			id:                   35,
+			name:                 "setrika",
+			expectStatus:         http.StatusOK,
+			expectBodyStartsWith: "{\"message\":\"success update",
+			expectBodyContains:   "setrika",
+		},
+	}
+
+	e := InitEcho()
+
+	for _, testCase := range testCases {
+		user := map[string]string{
+			"name":     testCase.name,
+			"email":    testCase.email,
+			"password": testCase.password,
+		}
+		data, _ := json.Marshal(user)
+		fmt.Println(string(data))
+		req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(string(data)))
+		req.Header.Set("Content-Type", "application/json")
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+		c.SetPath(testCase.path)
+		c.SetParamNames("id")
+		c.SetParamValues(strconv.Itoa(testCase.id))
+
+		if assert.NoError(t, UpdateUserByIdController(c)) {
+			assert.Equal(t, testCase.expectStatus, rec.Code)
+			body := rec.Body.String()
+			fmt.Println(body)
+			assert.True(t, strings.HasPrefix(body, testCase.expectBodyStartsWith))
+			assert.True(t, strings.Contains(body, testCase.expectBodyContains))
+		}
+	}
+}
+
+func TestDeleteUserByIdController(t *testing.T) {
+	var testCases = []struct {
+		testName             string
+		path                 string
+		id                   int
+		expectStatus         int
+		expectBodyStartsWith string
+	}{
+		{
+			testName:             "success",
+			path:                 "/users",
+			id:                   34,
+			expectStatus:         http.StatusOK,
+			expectBodyStartsWith: "{\"message\":\"success delete",
+		},
+	}
+	e := InitEcho()
+	req := httptest.NewRequest(http.MethodDelete, "/", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	for _, testCase := range testCases {
+		c.SetPath(testCase.path)
+		c.SetParamNames("id")
+		c.SetParamValues(strconv.Itoa(testCase.id))
+
+		if assert.NoError(t, DeleteUserByIdController(c)) {
+			assert.Equal(t, rec.Code, testCase.expectStatus)
+			body := rec.Body.String()
 			assert.True(t, strings.HasPrefix(body, testCase.expectBodyStartsWith))
 		}
 	}
